b8a84ea8a85f636a7634c764bbe7b7e05fad42df
github_username <you@domain.com>
2022-08-09 21:53:54 -0700

    frist commit

---
+++ oddEven.js
@@ -0,0 +1,24 @@
+// check even or odd with return
+
+function checkEvenOdd(num){
+    if (num % 2 == 0){
+        return "It is even number";
+    }
+    else {
+        return "It is odd number";
+    }
+}
+console.log(checkEvenOdd(73));
+
+// check even or odd with console.log
+
+function checkNumber (number){
+    if(number % 2 == 0){
+        console.log('It is even number')
+    }
+    else {
+        console.log('It is odd number')
+
+    }
+}
+console.log(checkNumber(72));
