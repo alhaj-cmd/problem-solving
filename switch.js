486a9ede5e97950cd930633d25dcbe90a767bd3d
github_username <you@domain.com>
2022-08-09 22:47:33 -0700

    switch function

---
+++ switch.js
@@ -0,0 +1,25 @@
+// switch-case grading system
+
+const grade = "A+"
+
+
+switch (grade){
+    case 'A+':
+    console.log('It is a very good studenst')
+    break;
+    case 'A-':
+    console.log('It is a good student')
+    break;
+    case 'B+':
+    console.log('It is a normal student')
+    break;
+    case 'B':
+    console.log('It is a  student')
+    break;
+    case 'F':
+    console.log('This subject is fail')
+    break;
+    default:
+        console.log('grade');
+    
+}
