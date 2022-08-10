8d2808924141ad41bb81c480f994f7ee062890a6
github_username <you@domain.com>
2022-08-09 22:33:16 -0700

    total price number

---
+++ priceProduct.js
@@ -0,0 +1,15 @@
+// write a program to find the total price of those product object.
+
+const a = {name:'laptop', color:'black', price:12000};
+const b = {name:'monitor', color:'gray', price:24000};
+const c = {name:'frize', color:'silver', price:28000};
+
+  //console.log(a.price);
+  //console.log(b.price);
+  //console.log(c.price);
+
+  const totalprice = (a.price + b.price + c.price );
+   console.log(totalprice);
+
+
+   
